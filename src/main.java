38ec1ff66f5cd700816d9ed6a85a6b0635b68a4d
mchrpt <mchrpt@yahoo.com>
2022-02-06 16:23:24 -0500

    reading file test.txt

--- src/main.java
+++ src/main.java
@@ -1,9 +1,25 @@
-
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello!");
+		
+		//Taken from https://www.w3schools.com/java/java_files_read.asp
+		try {
+		      File myObj = new File("test.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      
+		      while (myReader.hasNextLine()) {
+		    	  String data = myReader.nextLine();
+		    	  System.out.println(data);
+		      }
+		      
+		      myReader.close();
+		    
+		} catch (FileNotFoundException e) {
+		    	System.out.println("Error: file not found");
+		    	e.printStackTrace();
+		}
 	}
-
 }
