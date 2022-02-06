8109a18ba52b059d3f1e6f0f6e4c12e40eec4c1f
theaniartist <wsly@wpi.edu>
2022-02-06 16:44:58 -0500

    Add fitness function

--- src/main.java
+++ src/main.java
@@ -27,4 +27,25 @@ public class main {
 		    	e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Calculates the fitness of each individual
+	 * @param puzzleOption 1 for puzzle 1 or 2 for puzzle 2
+	 * @param bin1 the product of numbers
+	 * @param bin2 the sum of numbers
+	 * @param bin3 the calculated range
+	 * @param height height of the tower
+	 * @param totalCost cost to make the tower
+	 * @return the fitness score
+	 */
+	public static float fitnessFunction(int puzzleOption, float bin1, float bin2, float bin3, 
+			int height, int totalCost) {
+		float fitnessScore = 0;
+		if (puzzleOption == 1) {
+			fitnessScore = bin1 + bin2 + bin3;
+		} else if (puzzleOption == 2) {
+			fitnessScore = (float) (10 + Math.pow(height, 2) - totalCost);
+		}
+		return fitnessScore;
+	}
 }
